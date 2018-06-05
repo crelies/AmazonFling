3403526a5b8a605eb8a8160c9ec9210e9d7dc786
Elies, Christian <christian.elies@immonet.de>
2018-06-05 17:12:53 +0200

    chore(): updated amazon fling framework

--- Frameworks/AmazonFling.framework/Versions/A/Headers/AmazonFling.h
+++ Frameworks/AmazonFling.framework/Versions/A/Headers/AmazonFling.h
@@ -1,7 +1,18 @@
-#import <AmazonFling/AmazonFlingVersion.h>
-#import <AmazonFling/DiscoveryController.h>
-#import <AmazonFling/InstallDiscoveryController.h>
-#import <AmazonFling/MediaPlayerInfo.h>
-#import <AmazonFling/MediaPlayerStatus.h>
-#import <AmazonFling/RemoteInstallService.h>
-#import <AmazonFling/RemoteMediaPlayer.h>
+/**
+ *  AmazonFling.h
+ *
+ * Copyright (c) 2015-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * PROPRIETARY/CONFIDENTIAL
+ *
+ * Use is subject to license terms.
+ */
+
+#define AMAZONFLING_VERSION @"1.3.4"
+
+#import "DiscoveryController.h"
+#import "InstallDiscoveryController.h"
+#import "MediaPlayerInfo.h"
+#import "MediaPlayerStatus.h"
+#import "RemoteInstallService.h"
+#import "RemoteMediaPlayer.h"
